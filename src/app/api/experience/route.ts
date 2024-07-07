ee361eade3dcc1b6ea918fbc1b200c78db8ecb2f
Tejasva Jaitly <tejasvajaitly@gmail.com>
2024-07-06 18:24:25 -0700

    save experice api and fe

---
+++ src/app/api/experience/route.ts
@@ -0,0 +1,23 @@
+import { db } from "@/db/index";
+import { experiences } from "@/db/schema/experiences";
+import { v4 as uuidv4 } from "uuid";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    await db.insert(experiences).values({
+      id: uuidv4(),
+      companyName: body.companyName,
+      role: body.role,
+      metrics: body.metrics || null,
+      project: body.project,
+      userId: body.userId,
+    });
+    return Response.json({ data: "Experience saved successfully" });
+  } catch (err) {
+    console.log("Error", err);
+    return Response.json({
+      error: "There was an error saving the experience.",
+    });
+  }
+}
--- src/app/onboarding/experiences.tsx
+++ src/app/onboarding/experiences.tsx
@@ -1,8 +1,32 @@
+"use client";
+import { useAuth } from "@clerk/nextjs";
+
 function Experiences() {
+  const { userId } = useAuth();
+  const saveExperience = async () => {
+    try {
+      const response = await fetch("/api/experience", {
+        method: "POST",
+        body: JSON.stringify({
+          companyName: "Google",
+          role: "Software Engineer",
+          project: "Worked on the search engine",
+          metrics: "Increased search speed by 10%",
+          userId: userId,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div>
       <h1>Experience</h1>
       <p>Experience</p>
+      <button onClick={saveExperience}>save</button>
     </div>
   );
 }
